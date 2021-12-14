a86f3ff899bac970958f6ce9b5c3af01d290962d
Fatma Abdelhameed <94697516+FatmaAbdelhameed13@users.noreply.github.com>
2021-12-14 02:22:07 +0200

    Create QuickSort.java

---
+++ QuickSort.java
@@ -0,0 +1,31 @@
+public void Quick(int l,int r) {
+	if(r-l<=0)
+		return;
+	else {
+		long pivot=ar[r];
+		int partition=partitionIt(l,r,pivot);
+		Quick(l,partition-1);
+		Quick(partition+1,r);
+	}
+}
+public int partitionIt(int l,int r,long pivot) {
+	int lP=l;
+	int rP=r;
+	while(true) {
+		while(ar[++lP]<pivot);
+		while(rP>0&&ar[--rP]>pivot);
+		if(lP>=r)
+			break;
+		else {
+			swap(lP,rP);
+		}
+	}
+	swap(lP,r);
+	return l;
+}
+void swap(int indx1,int indx2) {
+	long temp=ar[indx1];
+	ar[indx1]=ar[indx2];
+    ar[indx2]=temp;
+}
+
